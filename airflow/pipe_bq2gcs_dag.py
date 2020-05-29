03cfaa70bb0be44db65934bcfa0eb35c4dd47fa7
Matias Piano <matias@globalfishingwatch.org>
2020-05-29 03:00:43 -0300

    debug lines

--- airflow/pipe_bq2gcs_dag.py
+++ airflow/pipe_bq2gcs_dag.py
@@ -93,6 +93,7 @@ class PipeBq2GcsDagFactory(DagFactory):
     def build(self, mode):
         dag_id = '{}_{}'.format(self.pipeline, mode)
         date_ranges=self.source_date_range()
+        print(date_ranges)
         export_config=self.config['export_config']
         export_config['jinja_query_parsed']=self.jinja_eval(export_config['jinja_query'], date_ranges.split(","))
         export_config['output_format']=export_config.get('output_format','CSV')
@@ -103,6 +104,7 @@ class PipeBq2GcsDagFactory(DagFactory):
             # Replace this if with a simple detect if the table is partitioned or not.
             if export_config['sensor_type']=='custom':
                 export_config['sensor_jinja_query_parsed']=self.jinja_eval(export_config['sensor_jinja_query'], date_ranges.split(","))
+                print(export_config)
                 sensor = table_custom_check('{sensor_jinja_query_parsed}'.format(**export_config))
             elif export_config['sensor_type'] == 'partitioning':
                 # Sharded expect to pass a dataset.table as sensor_jinja_query.
